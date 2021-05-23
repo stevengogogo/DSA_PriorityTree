bfaad5d8951785d4c275553daf6e914039781138
stevengogogo <stevengogogo@users.noreply.github.com>
2021-05-23 02:38:04 +0000

    deploy: c32d6250ef58526b96359e7abc91d23543a159b2

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['argmax_0',['argmax',['../utils_8h.html#a1fa7fd55f201216ebf7800abb1c427a8',1,'utils.c']]]
+];
---
+++ search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cprojecttemplate_1',['CProjectTemplate',['../index.html',1,'']]]
+];
