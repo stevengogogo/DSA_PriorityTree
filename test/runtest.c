c162d0b1ce080a481ddb76bd8d1b6b290efd80e3
Shao-Ting Chiu <r07945001@ntu.edu.tw>
2021-05-30 21:35:45 +0800

    Bug: insertion runtime error

--- test/runtest.c
+++ test/runtest.c
@@ -3,10 +3,11 @@
 #include "acutest.h"
 
 #include "test_utils.h"
-
+#include "test_insert.h"
 
 // Run TESTs 
 TEST_LIST = {
     {"Utility", test_utils},
+    {"Build treap", test_build},
     {NULL, NULL} // Terminate the test
 };
