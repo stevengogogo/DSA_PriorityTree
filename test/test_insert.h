a35a9e8c158d0c5eda04536def14ec5794750a64
stevengogogo <stevengogogo4321@gmail.com>
2021-05-30 14:07:38 +0000

    Bug: Null root after insertion

--- test/test_insert.h
+++ test/test_insert.h
@@ -4,6 +4,23 @@
 #include "treap.h"
 #include "acutest.h"
 
+void tree_validate(tnode* t){
+    if (t==NULL)
+        return;
+
+    //Priority
+    for(int i=0;i<2;i++)
+        TEST_CHECK(t->priority >= t->leaf[i]->priority);
+    
+    //Key
+    TEST_CHECK(t->size > t->leaf[LEFT]->size);
+    TEST_CHECK(t->size < t->leaf[RIGHT]->size);
+
+    tree_validate(t->leaf[LEFT]);
+    tree_validate(t->leaf[RIGHT]);
+}
+
+
 void test_build(void){
     init_nodes();
 
@@ -11,6 +28,8 @@ void test_build(void){
     int p[] = {1,2,4,-1};
 
     tnode* t = build_treap(p, len);
+    TEST_CHECK(t!=NULL);
+    tree_validate(t);
 
     clear_nodes();
 }
