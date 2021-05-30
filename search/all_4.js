b26f335031ac9b66c533dfeaf84e6a15c3fa2383
stevengogogo <stevengogogo@users.noreply.github.com>
2021-05-30 12:32:48 +0000

    deploy: 082d5bfe5a0c990ea52df559c826fe9e095fc4eb

--- search/all_4.js
+++ search/all_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['unpdatesize_6',['UnpdateSize',['../treap_8h.html#a06a966a483f4e59bd1c4b64898ca862b',1,'treap.c']]],
-  ['updateleafparent_7',['UpdateLeafParent',['../treap_8h.html#a08073cd1252b96f74bb5714c4347276e',1,'treap.c']]],
+  ['updateleafparent_6',['UpdateLeafParent',['../treap_8h.html#a08073cd1252b96f74bb5714c4347276e',1,'treap.c']]],
+  ['updatesize_7',['UpdateSize',['../treap_8h.html#a57648cfd389c2bf31fea6cef3648be5c',1,'treap.c']]],
   ['utils_2eh_8',['utils.h',['../utils_8h.html',1,'']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['unpdatesize_14',['UnpdateSize',['../treap_8h.html#a06a966a483f4e59bd1c4b64898ca862b',1,'treap.c']]],
-  ['updateleafparent_15',['UpdateLeafParent',['../treap_8h.html#a08073cd1252b96f74bb5714c4347276e',1,'treap.c']]]
+  ['updateleafparent_14',['UpdateLeafParent',['../treap_8h.html#a08073cd1252b96f74bb5714c4347276e',1,'treap.c']]],
+  ['updatesize_15',['UpdateSize',['../treap_8h.html#a57648cfd389c2bf31fea6cef3648be5c',1,'treap.c']]]
 ];
