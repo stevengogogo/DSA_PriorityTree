bece0d6a9c61d8845bc6439fb0c42b3992fd725b
stevengogogo <stevengogogo4321@gmail.com>
2021-06-06 17:39:23 +0000

    debug with main

--- src/main.c
+++ src/main.c
@@ -4,45 +4,35 @@
 #include "treap.h"
 #include <assert.h>
 
-void tree_validate(tnode* t, int* c){
-    if (t==NULL)
-        return;
-    *c = *c + 1;
-    for(int i=0;i<2;i++){
-        if(t->leaf[i] != NULL){
-            //Priority
-            assert(t->pt >= t->leaf[i]->pt);
-
-        }
-    }
-
-     //Key
-    if (t->leaf[LEFT] != NULL)
-    assert(t->key > t->leaf[0]->key);
-
-    if(t->leaf[RIGHT] != NULL)
-    assert(t->key < t->leaf[1]->key);
-
-    tree_validate(t->leaf[0], c);
-    tree_validate(t->leaf[1], c);
-}
 
 int main()
 {
-   init_nodes();
+    init_nodes();
+
     //Build tree
     int len = 10;
-    int p[] = {7,2,1,2,14,-12,2,2,2,2};
+    int p[] = {7,2,1,2,4,-12,2,2,2,2};
     tnode* t = build_treap(p, len);
 
-    Remove(&t);
+    /*DELETION*/
 
-  
+    //Original
+    int MAX = QueryLargest(t, 0, len-1);
 
-    Remove(&t);
-    Remove(&t);
-   
+    //Delete first item
+    Delete(&t, 1-1);
+    MAX = QueryLargest(t, 0, len-2);
 
+
+    //Delete fourth item
+    Delete(&t, 4-1); 
+    MAX = QueryLargest(t, 0, len-3);
+
+
+    //Delete All
+    for(int i=2;i<len;i++){
+        Delete(&t, 0);
+    }
 
     clear_nodes();
 
